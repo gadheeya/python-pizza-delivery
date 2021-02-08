ba88eeaeb69a0484b63aee5aef8699c98e1aa7ea
Ajey Gadheeya <67706606+gadheeya@users.noreply.github.com>
2021-02-08 06:46:04 +0000

    python pizza delivery

--- main.py
+++ main.py
@@ -7,7 +7,42 @@ extra_cheese = input("Do you want extra cheese? Y or N ")
 
 #Write your code below this line 👇
 
+bs = 15 #  smallPizza
+bm = 20 # mediuPizza
+bl = 25 # LargePizza
+bsp = 2 # small pepperoni
+bop = 3 # Other pepperoni
+bec = 1 # extra cheese
+
+pizza = 0 #selected pizza prise
+pepper = 0 #selected pepper prise
+cheese = 0 #selected cheese prise
+
+if  size == "S" :
+  pizza = bs
+elif size == "M" :
+  pizza = bm
+elif size == "L":
+  pizza = bl
+else:
+  pizza = 0
 
 
+if extra_cheese == "Y":
+  cheese = bec
+else :
+  cheese = 0
 
+if add_pepperoni == "Y" :
+  if size == "S" :
+    pepper = bsp
+  elif size == "M" :
+    pepper = bop
+  elif size == "L" :
+    pepper = bop
+  else :
+    pepper = 0
+    
+fbill = pizza + cheese + pepper
 
+print(f"Welcome to Python Pizza Deliveries!\nYour final bill is: ${fbill}.\n' ")
